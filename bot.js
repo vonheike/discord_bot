da117d3ee964cbcb403f24a2a3075ae32b9bc5b0
vonheike <39151914+vonheike@users.noreply.github.com>
2019-04-21 23:14:25 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+
+client.on('message', message => {
+  if (message.content === 'alexa play despacito'){
+    message.reply('no u nigga lmao yeet');
+  }
+});
+client.login(process.env.NTY5NjAzMjE3MTcyODU2ODMy.XLzaSg.OeNYopOp-51OGjKao-WNmBBNauk);
