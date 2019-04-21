b252566f6a736b9d30009c1c8f2a75ca8a593c18
vonheike <39151914+vonheike@users.noreply.github.com>
2019-04-21 23:35:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,11 @@
 const Discord = require('discord.js');
 
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('I am ready');
+});
+
 client.on('message', message => {
   if (message.content === 'alexa play despacito'){
     message.reply('no u nigga lmao yeet');
